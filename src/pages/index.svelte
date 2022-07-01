5c1db3d38e3eed74adadd3e03f51d61e682abfe6
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-07-01 13:49:12 -0300

    Add daisyui

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -8,7 +8,12 @@
 <h1 class="text-3xl font-bold underline">
     Routify Starter Template
 </h1>
-
+<ul class="steps">
+    <li class="step step-primary">Register</li>
+    <li class="step step-primary">Choose plan</li>
+    <li class="step">Purchase</li>
+    <li class="step">Receive Product</li>
+</ul>
 <p>
     To see an example app that shows off a lot of Routify's features, go to <a href="/example">/example</a>
 </p>
