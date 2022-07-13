375d5989ef12ef8bf89ad829d69dc3474f9c1dfb
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-07-13 12:11:32 -0300

    Add Header and footer componement

---
+++ src/UI/Footer.svelte
@@ -0,0 +1,3 @@
+<footer class="footer justify-items-center p-4 bg-neutral text-neutral-content">
+    <p>Copyright © 2022 - All right reserved</p>
+</footer>
---
+++ src/UI/Header.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+<div class="navbar bg-neutral text-neutral-content">
+  <div class="flex-1">
+    <a class="btn btn-ghost normal-case text-xl">Find Dev Job</a>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal p-0">
+        <li><a>Guyane</a></li>
+        <li><a>Reims</a></li>
+        <li><a>Tours</a></li>
+    </ul>
+  </div>
+</div>
